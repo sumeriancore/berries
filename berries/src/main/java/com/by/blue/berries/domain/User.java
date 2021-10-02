df8da77f388c7327ce11f7b6fda964ace60b1944
sumeriancore <SAusianikau@ibagroup.eu>
2021-10-02 18:57:07 +0300

    Autentification Start

--- berries/src/main/java/com/by/blue/berries/domain/User.java
+++ berries/src/main/java/com/by/blue/berries/domain/User.java
@@ -17,12 +17,48 @@ public class User {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "user_role")
+    private String userRole;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "username")
+    private String username;
+
     public User() {
     }
 
-    public User( String email, String password) {
+    public User(String email, String password) {
         this.email = email;
         this.password = password;
+        this.userRole = "USER";
+        this.active = true;
+        this.username = email;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public Integer getUserId() {
