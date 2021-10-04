ae45150209ab9fb4ee2c8efcd483db03db622328
sumeriancore <SAusianikau@ibagroup.eu>
2021-10-05 02:13:51 +0300

    Add CSS with Security and add logout

--- berries/src/main/java/com/by/blue/berries/config/WebSecurityConfig.java
+++ berries/src/main/java/com/by/blue/berries/config/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.by.blue.berries.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,16 +18,21 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
                 .authorizeRequests()
+                    .antMatchers("/static/**").permitAll()
                     .antMatchers("/login").permitAll()
-                    .anyRequest().authenticated()
+                    .anyRequest()
+                    .authenticated()
                     .and()
                 .formLogin()
                     .loginPage("/login")
+                    .defaultSuccessUrl("/main")
                     .permitAll()
                     .and()
                 .logout()
+                    .logoutSuccessUrl("/login")
                     .permitAll();
     }
 
@@ -33,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService() {
         UserDetails user =
                 User.withDefaultPasswordEncoder()
-                        .username("u")
+                        .username("u@q")
                         .password("l")
                         .roles("USER")
                         .build();
